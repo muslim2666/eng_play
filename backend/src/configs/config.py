96f74732bd2b1fa38277f315af89ee3523d1485c
MagomedDr <you@example.com>
2026-09-17 22:11:00 +0300

    add uv

---
+++ backend/src/configs/config.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=".env", 
+        env_file_encoding="utf-8",
+        extra_fields="ignore"
+    )
+
+    # Небольшая проверка: SQLAlchemy требует строку, а не объект URL
+    @field_validator("DATABASE_URL", mode="before")
+    def assemble_db_connection(cls, v: str) -> str:
+        if isinstance(v, str) and not v.startswith("postgresql+asyncpg://"):
+            raise ValueError("Ссылка должна начинаться с postgresql+asyncpg://")
+        return v
+
+settings = Settings()
